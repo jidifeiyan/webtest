2fa6ffcfd72f82b23c631b4ebb27c6d5ab55e426
jidifeiyan <252057285@qq.com>
2013-06-19 15:09:51 +0800

    commit jar

--- src/com/web/Main.java
+++ src/com/web/Main.java
@@ -1,14 +1,19 @@
 package com.web;
 
-public class Main {
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.struts2.ServletActionContext;
+
+public class Main{
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("update...");
-		System.out.println("use the username dengxl update...");
+		String path = Thread.currentThread().getContextClassLoader()
+		.getResource("").getPath();
+		System.out.println(path);
 	}
 
 }
